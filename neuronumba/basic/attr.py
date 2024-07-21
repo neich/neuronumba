e8b23f02245eb90ab351d4ab3e87ede978fe4fe8
Ignacio Martin <natx.mc@gmail.com>
2024-07-21 14:06:34 +0200

    Fix initialization and check for required attributes

--- neuronumba/basic/attr.py
+++ neuronumba/basic/attr.py
@@ -10,23 +10,29 @@ class Attr(object):
 class HasAttr(object):
     def __init__(self, **kwargs):
         cls = type(self)
-        attr_list = inspect.getmembers(cls)
-        for (name, value) in attr_list:
-            if isinstance(value, Attr):
-                if name in kwargs:
-                    setattr(self, name, kwargs.get(name))
-                else:
-                    setattr(self, name, value.default)
+        class_attrs = dict(inspect.getmembers(cls, lambda o: isinstance(o, Attr)))
+        # Initialize all attributes with its default
+        for name, value in class_attrs.items():
+            setattr(self, name, value.default)
+        # Set values of defined attributes in the arguments
+        for name, value in kwargs.items():
+            if name not in class_attrs:
+                raise AttributeError(f"Attribute <{name}> not found in <{cls.__name__}>!")
+            setattr(self, name, value)
 
-    def __set__(self, name, value):
-        cls = type(self)
-        assert hasattr(cls, name), f"Class {cls} has not attribute {name}"
-        attr = getattr(cls, name)
-        sc = type(value).__mro__
-        self.__setattr__(name, value)
 
     def configure(self, **kwargs):
         cls = type(self)
+        class_attrs = dict(inspect.getmembers(cls, lambda o: isinstance(o, Attr)))
         for name, value in kwargs.items():
-            if hasattr(self, name):
-                self.__set__(name, value)
+            if name not in class_attrs:
+                raise AttributeError(f"Attribute <{name}> not found in <{cls.__name__}>!")
+            setattr(self, name, value)
+        self._check_required()
+
+    def _check_required(self):
+        cls = type(self)
+        class_attrs = dict(inspect.getmembers(cls, lambda o: isinstance(o, Attr)))
+        for name, value in class_attrs.items():
+            if value.required and getattr(self, name) is None:
+                raise AttributeError(f"Attribute <{name}> of class <{cls.__name__}> has no value and is required to have one!")
