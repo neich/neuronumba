075d605e3e9a577e6b054a791ab10ef37d03715a
Ignacio Martin <natx.mc@gmail.com>
2024-07-21 14:07:49 +0200

    Use new get_numba_scheme() pattern

--- neuronumba/simulator/integrators/euler.py
+++ neuronumba/simulator/integrators/euler.py
@@ -1,8 +1,10 @@
-from numba import float64
+from numba import float64, njit
 from numba.experimental import jitclass
 import numpy as np
 import numpy.typing as npt
 
+from neuronumba.basic.attr import HasAttr, Attr
+from neuronumba.numba_tools.types import ArrF8_2d, ArrF8_1d
 from neuronumba.simulator.integrators.base_integrator import Integrator__init__
 import neuronumba.simulator.noise as nn_noise
 
@@ -25,29 +27,29 @@ class EulerMaruyama(object):
         :param stimulus:
         :return:
         """
-        d_state = model.dfun(state,coupling)
+        d_state = model.dfun(state, coupling)
         noise = self.noise.generate(state.shape[0])
         next_state = state + self.dt * (d_state + stimulus) + noise
         return next_state
 
 
-@jitclass
-class EulerDeterministic(object):
-    dt: float64
+class EulerDeterministic(HasAttr):
 
-    def __init__(self, dt=0.1):
-        self.dt = dt
+    dt = Attr(default=0.1, required=True)
 
-    def scheme(self, state: npt.ArrayLike, model, coupling, stimulus=None) -> np.ndarray:
-        """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-        :param state: state variables, shape (n_state__variables, n_rois)
-        :param dfun:
-        :param coupling:
-        :param local_coupling:
-        :param stimulus:
-        :return:
-        """
-        d_state = model.dfun(state,coupling)
-        next_state = state + self.dt * (d_state + stimulus)
-        return next_state
+    def get_numba_scheme(self, dfun):
+        dt = self.dt
+        stimulus = np.empty((1, 1))
+
+        @njit(float64[:,:](float64[:,:], float64[:], float64[:,:]))
+        def scheme(state: ArrF8_2d, model: ArrF8_1d, coupling: ArrF8_2d):
+            d_state = dfun(model, state, coupling)
+            if stimulus.shape[1] == state.shape[1]:
+                d_state = d_state + stimulus
+            next_state = state + dt * d_state
+            return next_state
+
+        return scheme
