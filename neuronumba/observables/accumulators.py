df1b94a431c0aa9b011e8d462543103c4519419c
Ignacio Martin <natx.mc@gmail.com>
2024-07-21 21:45:29 +0200

    New file

---
+++ neuronumba/observables/accumulators.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+from neuronumba.basic.attr import HasAttr
+
+
+class ObservableAccumulator(HasAttr):
+    def init(self, S, N):
+        raise Exception('observable accumulator not defined')
+
+
+class AveragingAccumulator(ObservableAccumulator):
+    def init(self, S, N):
+        return np.zeros((S, N, N))
+
+    def accumulate(self, FCs, nsub, signal):
+        FCs[nsub] = signal
+        return FCs
+
+    def postprocess(self, FCs):
+        return np.squeeze(np.mean(FCs, axis=0))
+
+
+class ConcatenatingAccumulator(ObservableAccumulator):
+    def init(self, S, N):
+        return np.array([], dtype=np.float64)
+
+    def accumulate(self, FCDs, nsub, signal):
+        FCDs = np.concatenate((FCDs, signal))  # Compute the FCD correlations
+        return FCDs
+
+    def postprocess(self, FCDs):
+        return FCDs  # nothing to do here
