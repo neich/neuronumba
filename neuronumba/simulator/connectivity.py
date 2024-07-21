f05290540865142888773aef7293bbe3f0cc3168
Ignacio Martin <natx.mc@gmail.com>
2024-07-21 14:07:21 +0200

    Add missing sttributes and initializations

--- neuronumba/simulator/connectivity.py
+++ neuronumba/simulator/connectivity.py
@@ -6,5 +6,16 @@ from neuronumba.basic.attr import HasAttr, Attr
 class Connectivity(HasAttr):
 
     weights = Attr(default=None, required=True)
+    lengths = Attr(default=None, required=True)
+    speed = Attr(default=1e6, required=False)
+    delays = Attr(default=None, required=False)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.delays = self.lengths / self.speed
+
+    @property
+    def n_rois(self):
+        return self.weights.shape[0]
 
 
