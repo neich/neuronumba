f4f54407536e0fc41bf65b6e9a906c647df0efac
Ignacio Martin <natx.mc@gmail.com>
2024-07-23 21:35:32 +0200

    Use _init_dependant()

--- neuronumba/simulator/noise.py
+++ neuronumba/simulator/noise.py
@@ -1,25 +1,21 @@
 from numba.experimental import jitclass
 import numpy as np
 
-
-class Noise(object):
-
-    def __init__(self):
-        pass
+from neuronumba.basic.attr import HasAttr, Attr
 
 
-@jitclass
+class Noise(HasAttr):
+    pass
+
+
 class Additive(Noise):
 
-    def __init__(self, sigmas):
-        """
+    sigmas = Attr(required=True)
+    n_state_vars = Attr(dependant=True)
 
-        :param sigmas: std deviation from normal distribution for noise generation.
-            Shape is (number_of_state_variables,)
-        """
-        super().__init__()
-        self.sigmas = sigmas
-        self._n_state_vars = sigmas.shape[0]
+    def _init_dependant(self):
+        super()._init_dependant()
+        self._n_state_vars = self.sigmas.shape[0]
 
     def generate(self, n_rois):
         """
@@ -27,8 +23,8 @@ class Additive(Noise):
         :param n_rois: number of regions of interest
         :return:
         """
-        noise = np.zeros((self._n_state_vars, n_rois))
-        for i in range(self._n_state_vars):
+        noise = np.zeros((self.n_state_vars, n_rois))
+        for i in range(self.n_state_vars):
             if self.sigmas[i] > 0.0:
                 noise[i, :] = np.random.normal(0.0, self.sigmas[i], n_rois)
         return noise
