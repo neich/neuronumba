6756b0121a45c8007e1286aa88b3d24598ab2df4
Ignacio Martin <natx.mc@gmail.com>
2024-07-23 21:33:55 +0200

    Drop use of @jitclass. USe Attr, HasAttr for Integrator base class

--- neuronumba/simulator/integrators/base_integrator.py
+++ neuronumba/simulator/integrators/base_integrator.py
@@ -1,17 +1,8 @@
-from numba import float64, jit
-from numba.experimental import jitclass
+from neuronumba.basic.attr import HasAttr, Attr
 
 
-@jitclass
-class Integrator:
-    dt: float64
+class Integrator(HasAttr):
+    dt = Attr(default=None, required=True)
 
-    def __init__(self, dt: float64):
-        self.dt = dt
-
-
-@jit(nopython=True)
-def Integrator__init__(instance, dt):
-    instance.dt = dt
 
 
