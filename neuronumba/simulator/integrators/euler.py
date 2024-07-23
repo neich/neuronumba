4f201b98ba6f98b5692ddc5e5b2d5209c508671b
Ignacio Martin <natx.mc@gmail.com>
2024-07-23 21:35:06 +0200

    Remove EulerMaruyama and use Integratr as base class. Remove unnecessary __init__() and configure()

--- neuronumba/simulator/integrators/euler.py
+++ neuronumba/simulator/integrators/euler.py
@@ -1,49 +1,18 @@
 from numba import float64, njit
-from numba.experimental import jitclass
 import numpy as np
-import numpy.typing as npt
 
 from neuronumba.basic.attr import HasAttr, Attr
 from neuronumba.numba_tools.types import ArrF8_2d, ArrF8_1d
-import neuronumba.simulator.noise as nn_noise
+from neuronumba.simulator.integrators.base_integrator import Integrator
 
 
-@jitclass
-class EulerMaruyama(object):
-    dt: float64
-
-    def __init__(self, dt=0.1, noise=nn_noise.Additive):
-        self.dt = dt
-        self.noise = noise
-
-    def scheme(self, state: npt.ArrayLike, model, coupling, stimulus=None) -> np.ndarray:
-        """
-
-        :param state: state variables, shape (n_state__variables, n_rois)
-        :param dfun:
-        :param coupling:
-        :param local_coupling:
-        :param stimulus:
-        :return:
-        """
-        d_state = model.dfun(state, coupling)
-        noise = self.noise.generate(state.shape[0])
-        next_state = state + self.dt * (d_state + stimulus) + noise
-        return next_state
-
-
-class EulerDeterministic(HasAttr):
-
-    dt = Attr(default=0.1, required=True)
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+class EulerDeterministic(Integrator):
 
     def get_numba_scheme(self, dfun):
         dt = self.dt
         stimulus = np.empty((1, 1))
 
-        @njit(float64[:,:](float64[:,:], float64[:], float64[:,:]))
+        @njit(float64[:, :](float64[:, :], float64[:], float64[:, :]))
         def scheme(state: ArrF8_2d, model: ArrF8_1d, coupling: ArrF8_2d):
             d_state = dfun(model, state, coupling)
             if stimulus.shape[1] == state.shape[1]:
@@ -54,13 +23,12 @@ class EulerDeterministic(HasAttr):
         return scheme
 
 
-class EulerStochastic(HasAttr):
+class EulerStochastic(Integrator):
 
-    dt = Attr(default=None, required=True)
     sigmas = Attr(default=None, required=True)
+    _sqrt_dt = Attr(dependant=True)
 
-    def configure(self, **kwargs):
-        super().configure(**kwargs)
+    def _init_dependant(self):
         self._sqrt_dt = np.sqrt(self.dt)
 
     def get_numba_scheme(self, dfun):
@@ -69,7 +37,7 @@ class EulerStochastic(HasAttr):
         sigmas = self.sigmas
         sqrt_dt = self._sqrt_dt
 
-        @njit(float64[:,:](float64[:,:], float64[:], float64[:,:]))
+        @njit(float64[:, :](float64[:, :], float64[:], float64[:, :]))
         def scheme(state: ArrF8_2d, model: ArrF8_1d, coupling: ArrF8_2d):
             d_state = dfun(model, state, coupling)
             if stimulus.shape[1] == state.shape[1]:
