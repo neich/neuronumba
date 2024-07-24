2036109d5ebce86affdf2b85dcc060bfed664d36
Ignacio Martin <natx.mc@gmail.com>
2024-07-24 12:07:58 +0200

    Use HasAttr as base class

--- neuronumba/simulator/models/base_model.py
+++ neuronumba/simulator/models/base_model.py
@@ -1,13 +1,5 @@
-from numba import types, typed, jit
-from numba.experimental import jitclass
-
-Model_spec = [
-    ('state_vars', types.ListType(types.unicode_type)),
-    ('n_state_vars', types.int32)
-]
+from neuronumba.basic.attr import HasAttr
 
 
-@jit(nopython=True)
-def Model_init(instance, state_vars):
-    instance.state_vars = typed.List(state_vars)
-    instance.n_state_vars = len(state_vars)
+class Model(HasAttr):
+    pass
