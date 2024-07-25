8fc444dc615bd247cdce74f3df5ee64a1f98d0d9
Ignacio Martin <natx.mc@gmail.com>
2024-07-25 17:27:46 +0200

    Fixed documentation

--- neuronumba/observables/base_observable.py
+++ neuronumba/observables/base_observable.py
@@ -2,16 +2,28 @@ import numpy as np
 from neuronumba.basic.attr import HasAttr, Attr
 
 
-# Abstract class for Observables. At the moment it has a main method "from_fMRI" that takes the signal and the filter
-# as parameters and outputs the result if computable (or None if some problem occurred). Each implementation has to
-# define "_compute_from_fMRI" method.
-#
-# NOTES: Implementation is as this to maximize the portability with the old class based library.
 class Observable(HasAttr):
+    """
+    Abstract class for Observables. At the moment it has a main method "from_fMRI" that takes the signal and the filter
+    as parameters and outputs the result if computable (or None if some problem occurred). Each implementation has to
+    define "_compute_from_fMRI" method.
+
+    NOTES: Implementation is as this to maximize the portability with the old class based library.
+
+    """
+
     ignore_nans = Attr(default=False)
     
-    # Main method to compute the Observable from an fMRI BOLD signal.
     def from_fmri(self, bold_signal):
+        """ Main method to compute the Observable from an fMRI BOLD signal.
+
+        Args:
+            bold_signal (ndarray): Bold signal with shape (n_rois, n_time_samples)
+
+        Returns:
+            dict: dictonary with the results
+        """
+        
         # ignoreNaN = 'ignore_nans' in kwargs and kwargs['ignore_nans']
         if not self.ignore_nans and np.isnan(bold_signal).any():
             return np.nan
