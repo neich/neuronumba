b1b2a59a3e092226a02b2ba1d3e834f2689284e6
Ignacio Martin <natx.mc@gmail.com>
2024-07-29 12:25:23 +0200

    Moved Naskar coupling to base Model class since its the default coupling model (linear) for most models

--- neuronumba/simulator/models/base_model.py
+++ neuronumba/simulator/models/base_model.py
@@ -1,3 +1,4 @@
+import numba as nb
 from neuronumba.basic.attr import HasAttr
 
 
@@ -13,4 +14,19 @@ class Model(HasAttr):
     def get_observed_sub(self, v_list: list[str] = None):
         v_list = v_list or []
         return {v: (self.observable_vars[v], i) for i, v in enumerate(v_list)}
+
+    def get_numba_coupling(self, g: nb.f8):
+        """
+        This is the default coupling for most models, linear coupling using the weights matrix
+
+        :param g: global linear coupling
+        :return:
+        """
+        @nb.njit #(nb.f8[:](nb.f8[:, :], nb.f8[:]))
+        def linear_coupling(weights, state):
+            r = weights @ state
+            return r * g
+
+        return linear_coupling
+
 
