2689d8a2924131d0cad2b225c35e09320f9d49b2
Ignacio Martin <natx.mc@gmail.com>
2024-07-29 14:59:02 +0200

    Changed module names to lowercase as per PEP 8

--- neuronumba/simulator/models/__init__.py
+++ neuronumba/simulator/models/__init__.py
@@ -1,2 +1,2 @@
-from neuronumba.simulator.models.Naskar2021 import Naskar2021
-from neuronumba.simulator.models.Deco2014 import Deco2014
+from neuronumba.simulator.models.naskar2021 import Naskar2021
+from neuronumba.simulator.models.deco2014 import Deco2014
