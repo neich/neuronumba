7a2fc61ccb3b2098b820e896b3b667364d6b54b5
Ignacio Martin <natx.mc@gmail.com>
2024-07-30 17:40:18 +0200

    Added new model

--- neuronumba/simulator/models/__init__.py
+++ neuronumba/simulator/models/__init__.py
@@ -1,3 +1,4 @@
 from neuronumba.simulator.models.model import Model
 from neuronumba.simulator.models.naskar2021 import Naskar2021
 from neuronumba.simulator.models.deco2014 import Deco2014
+from neuronumba.simulator.models.hopf import Hopf
