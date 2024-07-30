65c522f5d243236e70054e093e1bee1ebdb98b2d
Gustavo Patow <31112588+dagush@users.noreply.github.com>
2024-07-31 00:15:41 +0200

    added comments to the models...

--- neuronumba/simulator/models/hopf.py
+++ neuronumba/simulator/models/hopf.py
@@ -1,3 +1,35 @@
+# ==========================================================================
+# ==========================================================================
+# ==========================================================================
+# Normal form of a supercritical Hopf bifurcation
+#
+# General neural mass model known as the normal form of a Hopf bifurcation
+# (also known as Landau-Stuart Oscillators), which is the canonical model
+# for studying the transition from noisy to oscillatory dynamics.
+#
+#     .. [Kuznetsov_2013] Kuznetsov, Y.A. "Elements of applied bifurcation theory", Springer Sci & Business
+#     Media, 2013, vol. 112.
+#
+#     .. [Deco_2017]  Deco, G., Kringelbach, M.L., Jirsa, V.K. et al.
+#     The dynamics of resting fluctuations in the brain: metastability and its
+#     dynamical cortical core. Sci Rep 7, 3095 (2017).
+#     https://doi.org/10.1038/s41598-017-03073-5
+#
+#
+# The supHopf model describes the normal form of a supercritical Hopf bifurcation in Cartesian coordinates. This
+# normal form has a supercritical bifurcation at $a = 0$ with a the bifurcation parameter in the model. So for
+# $a < 0$, the local dynamics has a stable fixed point, and for $a > 0$, the local dynamics enters in a
+# stable limit cycle.
+#
+# The dynamic equations were taken from [Deco_2017]:
+#
+#         \dot{x}_{i} &= (a_{i} - x_{i}^{2} - y_{i}^{2})x_{i} - omega{i}y_{i} \\
+#         \dot{y}_{i} &= (a_{i} - x_{i}^{2} - y_{i}^{2})y_{i} + omega{i}x_{i}
+#
+#     where a is the local bifurcation parameter and omega the angular frequency.
+#
+# ==========================================================================
+# ==========================================================================
 from enum import IntEnum
 
 import numpy as np
@@ -17,7 +49,10 @@ class Hopf(Model):
     observable_vars = Model._build_var_dict([])
     n_observable_vars = len(observable_vars)
 
-    # Model variables
+    # ==========================================================================
+    # supercritical Hopf bifurcation Constants
+    # --------------------------------------------------------------------------
+    # Values taken from [Deco_2017]
     a = Attr(default=-0.5, attr_type=AttrType.Model)
     omega = Attr(default=0.3, attr_type=AttrType.Model)
     I_external = Attr(default=0.0, attr_type=AttrType.Model)
@@ -100,6 +135,30 @@ class Hopf(Model):
             y = state[1, :]
 
             pC = m[np.intp(P.I_external)] + 0j
+            # --------------------- From [Deco 2017] original code:
+            # First, we need to compute the term (in pseudo-LaTeX notation):
+            #       G Sum_i SC_ij (x_i - x_j) =
+            #       G (Sum_i SC_ij x_i + Sum_i SC_ij x_j) =
+            #       G ((Sum_i SC_ij x_i) + (Sum_i SC_ij) x_j)   <- adding some unnecessary parenthesis.
+            # This is implemented in Gus' code as:
+            #       wC = we * Cnew;  # <- we is G in the paper, Cnew is SC -> wC = G * SC
+            #       sumC = repmat(sum(wC, 2), 1, 2);  # <- for sum Cij * xj == sum(G*SC,2)
+            # Thus, we have that:
+            #       suma = wC*z - sumC.*z                 # this is sum(Cij*xi) - sum(Cij)*xj, all multiplied by G
+            #            = G * SC * z - sum(G*SC,2) * z   # Careful, component 2 in Matlab is component 1 in Python...
+            #            = G * (SC*z - sum(SC,2)*z)
+            # And now the rest of it...
+            # Remember that, in Gus' code,
+            #       omega = repmat(2*pi*f_diff',1,2);
+            #       omega(:,1) = -omega(:,1);
+            # so here I will call omega(1)=-omega, and the other component as + omega
+            #       zz = z(:,end:-1:1)  # <- flipped z, because (x.*x + y.*y)     # Thus, this zz vector is (y,x)
+            #       dz = a.*z + zz.*omega - z.*(z.*z+zz.*zz) + suma               # original formula in the code, using complex numbers z instead of x and y...
+            #          = zz * omega   +  z  * (a -  z.* z  - zz.* zz) + suma =    # I will be using vector notation here to simplify ASCII formulae... ;-)
+            #          = (y)*(-omega) + (x) * (a - (x)*(x) - (y)*(y)) + suma      # here, (x)*(x) should actually be (x) * (x,y)
+            #          =  x *(+omega)    y          y * y     x * x               #        y   y                     (y)
+            # ---------------------
+            # Calculate the input to nodes due to couplings
             xcoup = coupling[0, :]  # np.dot(SCT,x) - ink * x  # this is sum(Cij*xi) - sum(Cij)*xj
             ycoup = coupling[1, :]  # np.dot(SCT,y) - ink * y  #
             # Integration step
