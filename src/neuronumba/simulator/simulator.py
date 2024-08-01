e5a2c8d47378c81950d75e6d7af710160a551c58
Ignacio Martin <natx.mc@gmail.com>
2024-08-01 17:19:27 +0200

    Use neuronumba types. Fix new use of coupling function. Rearrange some code

--- src/neuronumba/simulator/simulator.py
+++ src/neuronumba/simulator/simulator.py
@@ -1,7 +1,8 @@
 import numba as nb
 
 from neuronumba.basic.attr import Attr, HasAttr
-from neuronumba.numba_tools.types import ArrF8_2d, ArrF8_1d
+from neuronumba.numba_tools import address_as_void_pointer, addr
+from neuronumba.numba_tools.types import NDA_f8_2d, NDA_f8_1d
 
 
 class Simulator(HasAttr):
@@ -23,7 +24,7 @@ class Simulator(HasAttr):
         self.integrator.configure()
         self.connectivity.configure()
         self.model.configure(n_rois=self.connectivity.n_rois)
-        self.coupling.configure(c_vars=self.model.c_vars, c_func=self.model.get_numba_coupling())
+        self.coupling.configure(c_vars=self.model.c_vars)
 
         dt = self.integrator.dt
         t_max = t_end - t_start
@@ -37,17 +38,23 @@ class Simulator(HasAttr):
         for m in self.monitors:
             m.configure(dt=dt, t_max=t_max, n_rois=n_rois)
 
-        c_couple = self.coupling.get_numba_couple()
+        c_couple = self.model.get_numba_coupling()
         c_update = self.coupling.get_numba_update()
+        c_sample = self.coupling.get_numba_sample()
         i_scheme = self.integrator.get_numba_scheme(self.model.get_numba_dfun())
         m_sample = self.monitors[0].get_numba_sample()
+        w_addr, w_shape, w_dtype = addr.get_addr(self.connectivity.weights)
 
+        # Uncomment this if you want to debug _sim_loop()
+        # w = self.connectivity.weights
         c_update(0, init_state)
 
         @nb.njit(nb.void(nb.intc, nb.f8[:, :], nb.f8[:, :]))
-        def _sim_loop(n_steps: nb.intc, state: ArrF8_2d, observed: ArrF8_2d):
+        def _sim_loop(n_steps: nb.intc, state: NDA_f8_2d, observed: NDA_f8_2d):
+            w = nb.carray(address_as_void_pointer(w_addr), w_shape, dtype=w_dtype)
             for step in range(1, n_steps + 1):
-                cpl = c_couple(step)
+                previous_state = c_sample(step)
+                cpl = c_couple(w, previous_state)
                 new_state, new_observed = i_scheme(state, cpl)
                 c_update(step, new_state)
                 m_sample(step-1, new_state, new_observed)
