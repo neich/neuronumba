29936b7f149abbb9e8ce11d07e8b52be13c1b962
Ignacio Martin <natx.mc@gmail.com>
2024-08-02 09:24:18 +0200

    Fix: check dataset check to return data accordingly. Rearrange code

--- src/neuronumba/numba_tools/hdf.py
+++ src/neuronumba/numba_tools/hdf.py
@@ -8,13 +8,18 @@ def loadmat(filename):
         f = h5py.File(filename, 'r')
         r = {}
         for k, ds in f.items():
-            r[k] = np.array(ds[:])
+            if len(ds.shape) == 0:
+                r[k] = ds[()]
+            else:
+                r[k] = np.array(ds[:])
         return r
-    except:
+    except Exception as e:
         return sio.loadmat(filename)
 
 
-def savemat(filename, mat):
-    with h5py.File(filename, 'w') as f:
-        for k, v in mat.items():
-            f.create_dataset(k, data=v)
+def savemat(filename, data):
+    f = h5py.File(filename, 'w')
+    for k, v in data.items():
+        f.create_dataset(k, data=v)
+    f.close()
+
