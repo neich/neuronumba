1d471dc84b6e3faeca2c66178a6353082f10ebcd
Ignacio Martin <natx.mc@gmail.com>
2024-08-03 11:21:14 +0200

    Add comment to specify matrix layout. Add n_rois as a dependant parameter

--- src/neuronumba/simulator/connectivity.py
+++ src/neuronumba/simulator/connectivity.py
@@ -3,9 +3,12 @@ from neuronumba.basic.attr import HasAttr, Attr
 
 class Connectivity(HasAttr):
 
+    # This is the connectivity matrix. A row specifies the INCOMING weights to a given regions of interest
     weights = Attr(default=None, required=True)
     lengths = Attr(default=None, required=True)
     speed = Attr(default=1e6, required=False)
+
+    n_rois = Attr(dependant=True)
 
     def _init_dependant(self):
         self.n_rois = self.weights.shape[0]
