b98364dcd73f7fd85236a29bbcd2221b4b5c211f
Ignacio Martin <natx.mc@gmail.com>
2024-08-03 22:37:31 +0200

    Changed input shape to fit all other apis

--- src/neuronumba/tools/filters.py
+++ src/neuronumba/tools/filters.py
@@ -18,25 +18,25 @@ class BandPassFilter(HasAttr):
     def filter(self, signal):
         """
 
-        :param signal: signal to filter with shape (n_rois, n_time_samples)
+        :param signal: signal to filter with shape (n_time_samples, n_rois)
         :return:
         """
-        n_rois, t_max = signal.shape
+        t_max, n_rois = signal.shape
         fnq = 1. / (2. * self.tr)  # Nyquist frequency
         Wn = [self.flp / fnq, self.fhi / fnq]  # butterworth bandpass non-dimensional frequency
         bfilt, afilt = butter(self.k, Wn, btype='band', analog=False, output='ba')  # construct the filter
         # bfilt = bfilt_afilt[0]; afilt = bfilt_afilt[1]  # numba doesn't like unpacking...
         signal_filt = np.empty(signal.shape)
-        for seed in range(n_rois):
-            if not np.isnan(signal[seed, :]).any():
-                ts = detrend(signal[seed, :]) if self.apply_detrend else signal[seed, :]
+        for n in range(n_rois):
+            if not np.isnan(signal[:, n]).any():
+                ts = detrend(signal[:, n]) if self.apply_detrend else signal[:, n]
                 ts = ts - np.mean(ts) if self.apply_demean else ts
 
                 if self.remove_artifacts:
                     ts[ts > 3. * np.std(ts)] = 3. * np.std(ts)  # Remove strong artefacts
                     ts[ts < -3. * np.std(ts)] = -3. * np.std(ts)  # Remove strong artefacts
 
-                signal_filt[seed, :] = filtfilt(bfilt, afilt, ts, padlen=3 * (max(len(bfilt),
+                signal_filt[:, n] = filtfilt(bfilt, afilt, ts, padlen=3 * (max(len(bfilt),
                                                                                   len(afilt)) - 1))  # Band pass filter. padlen modified to get the same result as in Matlab
 
             else:  # We've found problems, mark this region as "problematic", to say the least...
