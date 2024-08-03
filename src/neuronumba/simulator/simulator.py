08621d031bb14d94eb563250377eaa178c1bc13a
Ignacio Martin <natx.mc@gmail.com>
2024-08-03 11:29:59 +0200

    No need to use address_as_void_pointer() for read-only arrays. Just meake sure you have a copy in the closure. Weights matrix now in closure of coupling function.

--- src/neuronumba/simulator/simulator.py
+++ src/neuronumba/simulator/simulator.py
@@ -23,7 +23,7 @@ class Simulator(HasAttr):
         # Connect all components of the simulation
         self.integrator.configure()
         self.connectivity.configure()
-        self.model.configure(n_rois=self.connectivity.n_rois)
+        self.model.configure(weights=self.connectivity.weights)
         self.history.configure(c_vars=self.model.c_vars)
 
         dt = self.integrator.dt
@@ -32,7 +32,6 @@ class Simulator(HasAttr):
         n_steps = int((t_end - t_start) / dt)
         n_rois = self.connectivity.n_rois
         init_state = self.model.initial_state(n_rois)
-        init_observed = self.model.initial_observed(n_rois)
         self._state_shape = (int(self.model.n_state_vars), n_rois)
 
         for m in self.monitors:
@@ -44,21 +43,17 @@ class Simulator(HasAttr):
         i_scheme = self.integrator.get_numba_scheme(self.model.get_numba_dfun())
         # TODO: allow more than 1 monitor? Is really useful?
         m_sample = self.monitors[0].get_numba_sample()
-        w_addr, w_shape, w_dtype = addr.get_addr(self.connectivity.weights)
 
-        # Uncomment this if you want to debug _sim_loop()
-        # w = self.connectivity.weights
         h_update(0, init_state)
 
-        @nb.njit(nb.void(nb.intc, nb.f8[:, :], nb.f8[:, :]))
-        def _sim_loop(n_steps: nb.intc, state: NDA_f8_2d, observed: NDA_f8_2d):
-            w = nb.carray(address_as_void_pointer(w_addr), w_shape, dtype=w_dtype)
+        @nb.njit(nb.void(nb.intc, nb.f8[:, :]))
+        def _sim_loop(n_steps: nb.intc, state: NDA_f8_2d):
             for step in range(1, n_steps + 1):
-                previous_state = h_sample(step)
-                cpl = m_couple(w, previous_state)
+                previous_state_coupled = h_sample(step)
+                cpl = m_couple(previous_state_coupled)
                 new_state, new_observed = i_scheme(state, cpl)
                 h_update(step, new_state)
                 m_sample(step-1, new_state, new_observed)
                 state = new_state
 
-        _sim_loop(n_steps, init_state, init_observed)
+        _sim_loop(n_steps, init_state)
