262d5d6d59d44bf2ac49840d6dc45668b8becfbd
Gustavo Patow <31112588+dagush@users.noreply.github.com>
2024-08-05 10:34:30 +0200

    added comments, removed commented code and fixed some aesthetic things...

--- examples/hopf_test.py
+++ examples/hopf_test.py
@@ -294,16 +294,17 @@ if __name__ == '__main__':
     t_max_neuronal = (t_max - 1) * tr + 30
     t_warmup = t_max_neuronal/warmup_factor
 
-    bpf = BandPassFilter(k=2, flp=0.008, fhi=0.08, tr=2.0, apply_detrend=True, apply_demean=True)
+    # -------------------------- Setup Hopf frequencies
+    bpf = BandPassFilter(k=2, flp=0.008, fhi=0.08, tr=tr, apply_detrend=True, apply_demean=True)
     f_diff = filterps.filt_pow_spetra_multiple_subjects(timeseries, tr, bpf)  # baseline_group[0].reshape((1,52,193))
-    # f_diff(find(f_diff==0))=mean(f_diff(find(f_diff~=0)))
-    # f_diff[np.where(f_diff == 0)] = np.mean(f_diff[np.where(f_diff != 0)])
     omega = 2 * np.pi * f_diff
 
+    # -------------------------- Observable definition
     observables = {'phFCD': (PhFCD(), ConcatenatingAccumulator(), KolmogorovSmirnovStatistic())}
 
     all_fMRI = {s: d for s, d in enumerate(timeseries)}
 
+    # -------------------------- Process (or load) empirical data
     emp_filename = os.path.join(out_file_path, 'fNeuro_emp.mat')
     if os.path.exists(emp_filename):
         processed = hdf.loadmat(emp_filename)
@@ -311,10 +312,11 @@ if __name__ == '__main__':
         processed = process_empirical_subjects(all_fMRI, observables, bpf)
         hdf.savemat(emp_filename, processed)
 
-
+    # -------------------------- Load SC matrix
     mat0 = hdf.loadmat(SC_path)['SC_dbs80FULL']
     sc_norm = 0.2 * mat0 / mat0.max()
 
+    # -------------------------- Preprocessing pipeline!!!
     wes = np.arange(wStart, wEnd + wStep, wStep)
     optimal = preprocessing_pipeline(sc_norm, processed, observables, wes)
     # =======  Only for quick load'n plot test...
