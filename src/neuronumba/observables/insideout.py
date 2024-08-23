be6b205c6b6c3d5e99711d7e49e4c995239505e4
Ignacio Martin <natx.mc@gmail.com>
2024-08-23 11:38:55 +0200

    Edited to follow Python naming and documentation conventions

--- src/neuronumba/observables/insideout.py
+++ src/neuronumba/observables/insideout.py
@@ -14,78 +14,75 @@
 # Translated by Marc Gregoris
 # Ported by Gustavo Patow
 # =======================================================================
-# import warnings
+
 import numpy as np
-# from numba import jit
-# import WholeBrain.Observables.BOLDFilters as BOLDFilters
 from neuronumba.observables.base_observable import Observable
 from neuronumba.basic.attr import Attr
 import neuronumba.tools.matlab_tricks as tricks
 
 
-def InsideOUT(ts, NLAG):
-    FowRev = np.zeros((NLAG,))
-    AsymFow = np.zeros((NLAG,))
-    AsymRev = np.zeros((NLAG,))
+def inside_out(ts, nlag):
+    fow_rev = np.zeros((nlag,))
+    asym_fow = np.zeros((nlag,))
+    asym_rev = np.zeros((nlag,))
 
-    Tm = ts.shape[1]
-    for Tau in range(1,NLAG + 1):
+    tm = ts.shape[1]
+    for tau in range(1, nlag + 1):
 
         # Compute forward correlation
-        ts_1 = ts[:, 0:Tm-Tau]
-        ts_2 = ts[:, Tau:Tm]
-        FCtau_foward = tricks.corr(ts_1.T, ts_2.T)
+        ts_1 = ts[:, 0:tm-tau]
+        ts_2 = ts[:, tau:tm]
+        fc_tau_forward = tricks.corr(ts_1.T, ts_2.T)
 
         # Compute backwards correlation
-        ts_11 = ts[:, Tm-1: Tau - 1: -1].T
-        ts_22 = ts[:, Tm - Tau -1:: -1].T
-        FCtau_reversal = tricks.corr(ts_11, ts_22)
+        ts_11 = ts[:, tm-1: tau - 1: -1].T
+        ts_22 = ts[:, tm - tau -1:: -1].T
+        fc_tau_reversal = tricks.corr(ts_11, ts_22)
 
         # Squeeze to remove unneeded extra dimensions -> not really necessary!
-        FCtf = np.squeeze(FCtau_foward)
-        FCtr = np.squeeze(FCtau_reversal)
+        fc_tf = np.squeeze(fc_tau_forward)
+        fc_tr = np.squeeze(fc_tau_reversal)
 
-        Itauf = -0.5 * np.log(1 - FCtf**2)
-        Itaur = -0.5 * np.log(1 - FCtr**2)
-        Reference = (Itauf.flatten() - Itaur.flatten())**2
-        threshold = np.quantile(Reference, 0.95)  # Find the indices where the squared difference is greater than the 95th percentile
-        index = np.where(Reference > threshold)
+        itauf = -0.5 * np.log(1 - fc_tf**2)
+        itaur = -0.5 * np.log(1 - fc_tr**2)
+        reference = (itauf.flatten() - itaur.flatten())**2
+        # Find the indices where the squared difference is greater than the 95th percentile
+        threshold = np.quantile(reference, 0.95)
+        index = np.where(reference > threshold)
 
-        FowRev[Tau-1] = np.nanmean(Reference[index])
-        AsymFow[Tau-1] = np.mean(np.abs(Itauf - Itauf.T))
-        AsymRev[Tau-1] = np.mean(np.abs(Itaur - Itaur.T))
+        fow_rev[tau-1] = np.nanmean(reference[index])
+        asym_fow[tau-1] = np.mean(np.abs(itauf - itauf.T))
+        asym_rev[tau-1] = np.mean(np.abs(itaur - itaur.T))
 
-    return {"FowRev": FowRev, "AsymRev": AsymRev, "AsymFow": AsymFow}
+    return {"FowRev": fow_rev, "AsymRev": asym_rev, "AsymFow": asym_fow}
 
 
-# ================================================================================================================
-# Main Insideout class.
-# ================================================================================================================
 class InsideOut(Observable):
+    """
+    Main Insideout class.
 
-    NLAG = Attr(default=6, required=False)  # Number of taus (lag values) to compute
+    """
 
-    def _compute_from_fmri(self, fMRI):
-        cc = InsideOUT(fMRI.T, self.NLAG)
+    nlag = Attr(default=6, required=False)  # Number of taus (lag values) to compute
+
+    def _compute_from_fmri(self, fmri):
+        cc = inside_out(fmri.T, self.nlag)
         return cc
 
-    # ================================================================================================================
-    # calculate_Tauwinner: This method, technically, is not part of the observable, but to keep things coherent, and
-    # as it is part of the Framework, we keep it here
-    # Computes the group with the max mean FowRev.
-    # Dataset is a dict of group labels, and for each label a list of the subjectIDs in FowRev
-    # ================================================================================================================
-    def calculate_Tauwinner(self, dataset, FowRev):
+    def calculate_tauwinner(self, dataset, fow_rev):
+        """
+        This method, technically, is not part of the observable, but to keep things coherent, and
+        as it is part of the Framework, we keep it here.
+        :param dataset: dict of group labels, and for each label a list of the subjectIDs in fow_rev
+        :param fow_rev:
+        :return: the group with the max mean fow_rev.
+        """
         max_means = []
         for group in dataset:
             subjects = dataset[group]
-            FowRevMatr = np.zeros((self.NLAG, len(subjects)))
+            fow_rev_matr = np.zeros((self.nlag, len(subjects)))
             for pos, subject in enumerate(subjects):
-                FowRevMatr[:, pos] = FowRev[subject]
-            max_means.append(np.argmax(np.mean(FowRevMatr, 1)))
-        Tauwinner = np.round(np.mean(max_means))
-        return int(Tauwinner)
-
-# ================================================================================================================
-# ================================================================================================================
-# ================================================================================================================EOF
+                fow_rev_matr[:, pos] = fow_rev[subject]
+            max_means.append(np.argmax(np.mean(fow_rev_matr, 1)))
+        tau_winner = np.round(np.mean(max_means))
+        return int(tau_winner)
