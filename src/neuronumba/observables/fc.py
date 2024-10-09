2a4a5af1da6ef35250e496564d0e465da719c184
Gustavo Patow <31112588+dagush@users.noreply.github.com>
2024-10-09 17:44:45 +0200

    Added FC

---
+++ src/neuronumba/observables/fc.py
@@ -0,0 +1,18 @@
+import numpy as np
+# import numba as nb
+
+from neuronumba.observables.base_observable import Observable
+# from neuronumba.basic.attr import Attr
+
+
+class FC(Observable):
+    """
+    Main FC class.
+
+    Args:
+        fmri (ndarray): Bold signal with shape (n_rois, n_time_samples)
+    """
+
+    def _compute_from_fmri(self, fmri):
+        cc = np.corrcoef(fmri.T, rowvar=False)  # Pearson correlation coefficients
+        return cc
