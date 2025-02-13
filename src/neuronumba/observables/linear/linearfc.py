52de9a268d618e2992a95c4ca02f8fcc4bc087d7
Albert Junca <albert.junca.sabria@gmail.com>
2025-02-13 12:20:14 +0100

    Minor changes

--- src/neuronumba/observables/linear/linearfc.py
+++ src/neuronumba/observables/linear/linearfc.py
@@ -5,6 +5,10 @@ from neuronumba.basic.attr import HasAttr, Attr
 from neuronumba.observables.base_observable import Observable
 from neuronumba.tools.matlab_tricks import correlation_from_covariance
 
+# Necessary if using the "slycot" versions of the solver
+# import control
+# import numpy as np
+
 class LinearFC(Observable):
     A = Attr(default=None)
     Qn = Attr(default=None)
@@ -24,6 +28,14 @@ class LinearFC(Observable):
         N = int(self.A.shape[0] / 2)
         # Solves the Lyapunov equation: A*X + X*Ah = Q, with Ah the conjugate transpose of A
         CVth = linalg.solve_continuous_lyapunov(self.A, -self.Qn)
+
+        # There are two other options to compute the solution to the equation, using the same slycot library under matlab implementation.
+        # A) Using the lyap "sylvester" version. Under the hood is calling slycot function "sb04md"
+        # Aconjtrans = np.atleast_2d(self.A).T.conj()
+        # CVth = control.lyap(self.A, Aconjtrans, self.Qn, method='slycot')
+        # B) Using the lyap "lyapunov" version. Under the hood is calling slycot function "sb03md"
+        # CVth = control.lyap(self.A, self.Qn, method='slycot')
+
         # simulated FC
         FCth = correlation_from_covariance(CVth)
         # Functional connectivity matrix (FC)
