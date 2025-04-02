f92a89f43308be6d07ecdc62ea84ffb7babc530b
Albert Junca <albert.junca.sabria@gmail.com>
2025-04-02 12:27:56 +0200

    Move HFreq to an observable + change FDT to FdtDeco2023

---
+++ src/neuronumba/observables/hfreq.py
@@ -0,0 +1,29 @@
+import numpy as np
+from neuronumba.basic.attr import Attr
+from typing import Union
+from neuronumba.observables.base_observable import Observable
+from neuronumba.tools import filterps
+
+class HFreq(Observable):
+    """
+    Observable class to compute given a group, the h-frequencies for each node
+    """
+
+    # tr in ms
+    tr = Attr(default=None)
+    # The array or dictionary of the group that the frequencies will be extracted from. 
+    # Each frmi on the array/dict should be in (time, nodes)
+    group_fmri = Attr(default=None)
+    # The version of filterps to use 
+    filterps_version = Attr(default=filterps.FiltPowSpetraVersion.v2021)
+
+    def _compute(self):
+        if not isinstance(self.tr, float):
+            raise TypeError(f'Parameter "tr" must be float, got {type(self.tr).__name__}')
+        if not isinstance(self.group_fmri, (np.ndarray, dict)):
+            raise TypeError(f'Parameter "group_fmri" must be either np.ndarray or dict, got {type(self.group_fmri).__name__}')
+        if not isinstance(self.filterps_version, filterps.FiltPowSpetraVersion):
+            raise TypeError(f'Parameter "filterps_version" must be FilterPowSpetraVersion, got {type(self.filterps_version).__name__}')
+
+        f_diff = filterps.filt_pow_spetra_multiple_subjects(self.group_fmri, self.tr, self.filterps_version)
+        return 2 * np.pi * f_diff  # omega
