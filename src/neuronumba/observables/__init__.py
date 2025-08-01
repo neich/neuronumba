e53b54b47292acb29ba9fda1d093cc3709188e62
Ignacio Martín <natx.mc@gmail.com>
2025-08-01 15:43:42 +0200

    Add import for SwFCD observable in the observables module

--- src/neuronumba/observables/__init__.py
+++ src/neuronumba/observables/__init__.py
@@ -1,5 +1,6 @@
 from neuronumba.observables.fc import FC
 from neuronumba.observables.ph_fcd import PhFCD
+from neuronumba.observables.sw_fcd import SwFCD
 from neuronumba.observables.turbulence import Turbulence
 from neuronumba.observables.turbulence2 import Information_cascade, Information_transfer
 from neuronumba.observables.hfreq import HFreq
