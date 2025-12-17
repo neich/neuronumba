Fix FIC computation, g is already used in linear coupling

src/neuronumba/fitting/fic/fic.py
@@ -7,15 +7,15 @@ from neuronumba.simulator.simulator import simulate_nodelay
 class FIC(HasAttr):
     dim = Attr(required=True)
     
-    def compute_J(self, sc, g):
+    def compute_J(self, sc):
         raise NotImplementedError
     
     
 class FICHerzog2022(FIC):
     alpha = Attr(default=0.75)
     
-    def compute_J(self, sc, g):
-        J = self.alpha * g * np.sum(sc, axis=0) + 1
+    def compute_J(self, sc):
+        J = self.alpha * np.sum(sc, axis=0) + 1
         return J
 
 class FICDeco2014(FIC):
@@ -107,7 +107,7 @@ class FICDeco2014(FIC):
         if self.very_verbose: print("]")
         return N - num_above_error
 
-    def compute_J(self, sc, g):
+    def compute_J(self, sc):
         # simulation fixed parameters:
         # ----------------------------
         dt = 0.1
@@ -162,7 +162,7 @@ class FICDeco2014(FIC):
                 if self.verbose: print(', ', end='', flush=True)
 
         if self.verbose:
-            print("Final (we={}): {} trials, with {}/{} nodes solved at trial {}".format(g, k, bestJCount, N, bestTrial))
+            print("Final: {} trials, with {}/{} nodes solved at trial {}".format(k, bestJCount, N, bestTrial))
         if self.verbose:
             print('DONE!') if flagJ == N else print('FAILED!!!')
         return bestJ

src/neuronumba/simulator/models/montbrio.py
@@ -95,7 +95,7 @@ class Montbrio(LinearCouplingModel):
         self.J_G_ei = self.J_ei + self.g_ei * np.log(self.a_i)
         self.J_G_ii = self.J_ii + self.g_ii * np.log(self.a_i)
         if self.auto_fic:
-            self.J = FICHerzog2022().compute_J(self.weights, self.g)
+            self.J = self.J * FICHerzog2022().compute_J(self.weights)
 
     def initial_state(self, n_rois: int) -> np.ndarray:
         """Initialize state variables.